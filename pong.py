51496b3141d28c0fdce21b930e34edf3f405e1a1
Emily Landgrebe <emilandgrebe@gmail.com>
2019-12-13 11:53:03 -0600

    Set up basic paddles

--- pong.py
+++ pong.py
@@ -1,15 +1,39 @@
 import turtle
 
-# Creating window
+  # Creating window
 wn = turtle.Screen()
-# Title to game
+  # Title to game
 wn.title("Python Pong")
-# Color of background
+  # Color of background
 wn.bgcolor("black")
-# Size of window
+  # Size of window
 wn.setup(width=800, height=600)
-# Speeds up game
+  # Speeds up game
 wn.tracer(0)
+
+# PADDLE A - LEFT
+paddle_a = turtle.Turtle()
+  # Speed of animation-to the max possible speed
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+  # removes the default line while moving
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+
+# PADDLE B - RIGHT
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# BALL
+
 
 # Main game loop
 while True:
